12afbf737f211eeb6980ae043fc40e01a463602c
Pablo Querales <pabloquerales20@gmail.com>
2025-08-15 22:22:33 +0200

    Initial commit - proyecto limpio

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+	return (
+		<>
+			<div className="bg-amber-950 h-screen w-full flex justify-center items-center flex-col">
+				<p className="text-white">Pagina en Construcción</p>
+				<span className="text-white">Pablo Querales</span>
+			</div>
+		</>
+	);
+}
+
+export default App;
